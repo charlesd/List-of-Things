6e894d281fe56e4e4a085d9c80f072a1d7a4c125
charlesd <nicmiu@gmail.com>
2021-07-12 18:41:39 -0400

    Added remove and move-up buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
+import "./App.css"
 
 export default function App() {
   const [items, setItems] = useState([])
   const [text, setText] = useState("")
+
+	useEffect(() => {
+    if ('items' in localStorage)
+      setItems(JSON.parse(localStorage['items']))
+    if ('text' in localStorage)
+      setText(JSON.parse(localStorage['text']))
+	}, [])
+
+	useEffect(() => {
+		localStorage['items'] = JSON.stringify(items)
+		localStorage['text'] = JSON.stringify(text)
+	}, [items, text])
 
   const addItem = () => {
     setItems(prevItems => [...prevItems, text])
     setText("")
   }
 
+  const removeItem = (index) => {
+    setText(items[index])
+    setItems(prevItems => prevItems.filter((item, i) => i !== index))
+  }
+
+  const moveUpItem = (index) => {
+    setItems(prevItems => {
+      let item = prevItems[index]
+      let new_items = [...prevItems]
+      new_items.splice(index, 1)
+      new_items.splice(index-1, 0, item)
+      return new_items
+    })
+  }
+
   return (
     <div className="App">
       <h1>My List of Things</h1>
-      <ul>
-        {items.map(item => (
-          <li>{item}</li>
-        ))}
+      <ul style={{
+          border: 'solid 1px red'
+      }}>
+        {items.map((item, index) => 
+          <li>
+            <button onClick={() => removeItem(index)}>
+              x
+            </button>
+            <button disabled={index===0} onClick={() => moveUpItem(index)}>
+              ^
+            </button>
+            {item}
+          </li>
+        )}
       </ul>
       <TextField
         value={text}
